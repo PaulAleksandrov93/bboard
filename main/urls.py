7e742ad2446a412164afc8a7160836a4dd798075
PaulAleksandrov93 <aleksandrov.pavel93@gmail.com>
2023-03-26 22:30:55 +0300

    create other pages

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
+from .views import other_page
 from .views import index
 
 app_name = 'main'
 
 urlpatterns = [
-    path('', index, name='index')
+    path('<str:page>/', other_page, name='other'),
+    path('', index, name='index'),
 ]
