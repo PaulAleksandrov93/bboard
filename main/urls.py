b242903e5910238eee7bea64ce8fbb6ebd21fb86
PaulAleksandrov93 <aleksandrov.pavel93@gmail.com>
2023-03-28 21:29:42 +0300

    created a login page

--- main/urls.py
+++ main/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 
 from .views import other_page
 from .views import index
+from .views import BBLoginView
 
 app_name = 'main'
 
 urlpatterns = [
+    path('accounts/login/', BBLoginView.as_view(), name='login'),
     path('<str:page>/', other_page, name='other'),
     path('', index, name='index'),
 ]
