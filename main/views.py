707ae9b625809a2db3558b9d1c60e728e7e25eb8
PaulAleksandrov93 <aleksandrov.pavel93@gmail.com>
2023-04-05 22:53:20 +0300

    create controller for user registration

--- main/views.py
+++ main/views.py
@@ -6,14 +6,16 @@ from django.contrib.auth.views import LoginView
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import LogoutView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic.edit import UpdateView
+from django.views.generic.edit import UpdateView, CreateView
 from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.views import PasswordChangeView
+from django.views.generic.base import TemplateView
 
 from .models import AdvUser
 from .forms import ChangeUserInfoForm
+from .forms import RegisterUserForm
 
 def index(request):
     return render(request, 'main/index.html')
@@ -58,3 +60,12 @@ class BBPasswordChangeView(SuccessMessageMixin, LoginRequiredMixin, PasswordChan
     template_name = 'main/password_change.html'
     success_url = reverse_lazy('main:profile')
     success_message = 'Пароль пользователя изменен'
+
+class RegisterUserView(CreateView):
+    model = AdvUser
+    template_name = 'main/register_user.html'
+    form_class = RegisterUserForm
+    success_url = reverse_lazy('main:register_done')
+
+class RegisterDoneView(TemplateView):
+    template_name = 'main/register_done.html'
