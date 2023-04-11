d920de5bbfd4cb8f3c8c845cdd06f3351f0e3eb1
PaulAleksandrov93 <aleksandrov.pavel93@gmail.com>
2023-04-11 22:30:46 +0300

    added controller for delete user

--- main/forms.py
+++ main/forms.py
@@ -27,12 +27,12 @@ class RegisterUserForm(forms.ModelForm):
     
     def clean(self):
         super().clean()
-        password1 = self.clean_password[password1]
-        password2 = self.clean_password[password2]
+        password1 = self.cleaned_data['password1']
+        password2 = self.cleaned_data['password2']
         if password1 and password2 and password1 != password2:
             errors = {'password2': ValidationError(
                 'Введенные пароли не совпадают', code='password_mismatch')}
-
+            raise ValidationError(errors)
     def save(self, commit=True):
         user = super().save(commit=False)
         user.set_password(self.cleaned_data['password1'])
--- main/views.py
+++ main/views.py
@@ -2,17 +2,18 @@ from django.shortcuts import render
 from django.http import HttpResponse, Http404
 from django.template import TemplateDoesNotExist
 from django.template.loader import get_template
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, PasswordChangeView, LogoutView
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.views import LogoutView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic.edit import UpdateView, CreateView
+from django.views.generic.edit import UpdateView, CreateView, DeleteView
 from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
 from django.shortcuts import get_object_or_404
-from django.contrib.auth.views import PasswordChangeView
 from django.views.generic.base import TemplateView
 from django.core.signing import BadSignature
+from django.contrib.auth import logout
+from django.contrib import messages
+
 
 from .models import AdvUser
 from .forms import ChangeUserInfoForm
@@ -87,3 +88,22 @@ class RegisterUserView(CreateView):
 class RegisterDoneView(TemplateView):
     template_name = 'main/register_done.html'
 
+class DeleteUserView(DeleteView, LoginRequiredMixin):
+    model = AdvUser
+    template_name = 'main/delete_user.html'
+    success_url = reverse_lazy('main:index')
+
+    def setup(self, request, *args, **kwargs):
+        self.user_id = request.user.pk
+        return super().setup(request, *args, **kwargs)
+    
+    def post(self, request, *args, **kwargs):
+        logout(request)
+        messages.add_message(request, messages.SUCCESS, 'Пользователь удален')
+        
+        return super().post(request, *args, **kwargs)
+    
+    def get_object(self, queryset=None):
+        if not queryset:
+            queryset = self.get_queryset()
+        return get_object_or_404(queryset, pk=self.user_id)
