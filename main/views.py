296ff2286b9c203faea54b94e49d51c1653b63aa
PaulAleksandrov93 <aleksandrov.pavel93@gmail.com>
2023-04-29 22:50:44 +0300

    edited base template and added some fix

--- main/views.py
+++ main/views.py
@@ -107,3 +107,6 @@ class DeleteUserView(DeleteView, LoginRequiredMixin):
         if not queryset:
             queryset = self.get_queryset()
         return get_object_or_404(queryset, pk=self.user_id)
+
+def by_rubric(request,  pk):
+    pass
